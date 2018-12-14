8e43178dd936694d0d50b3af9183d0dc57b05db8
Victor Wagner <victorwdr@gmail.com>
2018-12-14 18:44:26 -0300

    Initial Commit

---
+++ scripts/processElementData.js
@@ -0,0 +1,28 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.elementUrls["_-qSmQfaTEei1V6SodS_47g"] = ["Realizar modificações","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/tasks/realizar_modificacoes_3562BB11.html"];
+contentPage.processPage.elementUrls["_-qT0YvaTEei1V6SodS_47g"] = ["Produtor","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/roles/produtor_F2277740.html"];
+contentPage.processPage.elementUrls["_-qPi8PaTEei1V6SodS_47g"] = ["Consumir conteúdo","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/tasks/consumir_conteudo_763CEE37.html"];
+contentPage.processPage.elementUrls["_-qW3svaTEei1V6SodS_47g"] = ["Requisição de mudança","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/workproducts/requisicao_de_mudanca_EBB4215B.html"];
+contentPage.processPage.elementUrls["_-qUbcPaTEei1V6SodS_47g"] = ["Conteúdo consumido","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/workproducts/conteudo_consumido_F24F5506.html"];
+contentPage.processPage.elementUrls["_-qWQoPaTEei1V6SodS_47g"] = ["Modificação realizada","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/workproducts/modificacao_realizada_844EDED8.html"];
+contentPage.processPage.elementUrls["_-qR_MPaTEei1V6SodS_47g"] = ["Entregar modificações","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/tasks/entregar_modificacoes_D3031B6B.html"];
+contentPage.processPage.elementUrls["_-qTNUPaTEei1V6SodS_47g"] = ["Consumidor","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/roles/consumidor_72839B17.html"];
+contentPage.processPage.elementUrls["_-qT0YPaTEei1V6SodS_47g"] = ["Produtor","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/roles/produtor_2A24378.html"];
+contentPage.processPage.elementUrls["_-qSmQPaTEei1V6SodS_47g"] = ["Identificar problema","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/tasks/identificar_problema_13532A5.html"];
+contentPage.processPage.elementUrls["_-qW3sPaTEei1V6SodS_47g"] = ["Problema","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/workproducts/problema_C1486031.html"];
+contentPage.processPage.elementUrls["_-qSmQvaTEei1V6SodS_47g"] = ["Requisitar alteração","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/tasks/requisitar_alteracao_63445F72.html"];
+contentPage.processPage.elementUrls["_s1DDAfaTEei1V6SodS_47g"] = ["Fluxo de alterações no CMS Plone","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/deliveryprocesses/Fluxo de alterações no CMS Plone_CF6C3A0D.html"];
+contentPage.processPage.elementUrls["_-qWQovaTEei1V6SodS_47g"] = ["Modificação entregue","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/workproducts/modificacao_entregue_358D8037.html"];
+contentPage.processPage.elementUrls["_-qT0ZPaTEei1V6SodS_47g"] = ["Administrador","Realizando alterações no Sistema Gerenciador de Conteúdo - Plone/roles/administrador_20B0829.html"];
